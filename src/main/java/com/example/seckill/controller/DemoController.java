ca0ffe1a0c1443f2f17996f1b7fa12845d712bf9
EIMadrigal <renjingtaofc@163.com>
2023-04-26 17:12:12 +0800

    hello demo

--- src/main/java/com/example/seckill/controller/DemoController.java
+++ src/main/java/com/example/seckill/controller/DemoController.java
@@ -1,2 +1,26 @@
-package com.example.seckill.controller;public class DemoController {
+package com.example.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 测试
+ *
+ * @author jingtao
+ * @since 1.0.0
+ */
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    /**
+     * 测试页面跳转
+     */
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "xxxx");
+        return "hello";
+    }
 }
